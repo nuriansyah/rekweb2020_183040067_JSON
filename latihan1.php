2125384369f6e8fd1a938dc769f3f65fc3459e96
nuriansyah <nuriansyah09@gmail.com>
2020-11-24 07:54:40 +0700

    +++LATIHAN 1&2+++

---
+++ latihan1.php
@@ -0,0 +1,17 @@
+<?php
+
+$mahasiswa = [
+    [
+        "nama"  => "Muhammad Nuriansyah MN",
+        "nrp"   => "183040067",
+        "email" => "weswley@gmail.com"
+    ],
+    [
+        "nama"  => "Muhammad Nuriansyah MN",
+        "nrp"   => "183040067",
+        "email" => "weswley@gmail.com"
+    ]
+];
+
+$data = json_encode($mahasiswa);
+echo $data;
